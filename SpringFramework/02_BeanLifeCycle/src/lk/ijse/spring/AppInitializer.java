748287ff5b370aa8fdf3594c59c5aa6e403e8ec0
sanuv9683 <sanuv9683@gmail.com>
2022-12-19 16:11:05 +0530

    Content Updated with Shut-Down Hooks

--- SpringFramework/02_BeanLifeCycle/src/lk/ijse/spring/AppInitializer.java
+++ SpringFramework/02_BeanLifeCycle/src/lk/ijse/spring/AppInitializer.java
@@ -1,13 +1,37 @@
 package lk.ijse.spring;
 
 import lk.ijse.spring.config.AppConfig;
+import lk.ijse.spring.pojo.SpringBeanOne;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AppInitializer {
     public static void main(String[] args) {
+
+
+
         AnnotationConfigApplicationContext ctx= new AnnotationConfigApplicationContext();
         ctx.register(AppConfig.class);
         ctx.refresh();
-        ctx.close();
+
+        //Let's request spring bean One from container
+        SpringBeanOne bean = ctx.getBean(SpringBeanOne.class);
+        System.out.println(bean);
+
+
+        //Run Something just before jvm is going to shut-down (Hooking Process)
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            @Override
+            public void run() {
+                ctx.close();
+            }
+        }));
+        //Spring Alternative for above code
+        ctx.registerShutdownHook();
+
+
+        SpringBeanOne bean2 = ctx.getBean(SpringBeanOne.class);
+        System.out.println(bean2);
+
+
     }
 }
