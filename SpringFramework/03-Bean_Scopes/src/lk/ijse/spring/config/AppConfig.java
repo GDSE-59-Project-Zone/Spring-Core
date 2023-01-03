8fc9323d4b00764bae1b7f5143adc8434fe258cd
sanuv9683 <sanuv9683@gmail.com>
2023-01-03 09:04:27 +0530

    Content Updated

---
+++ SpringFramework/03-Bean_Scopes/src/lk/ijse/spring/config/AppConfig.java
@@ -0,0 +1,10 @@
+package lk.ijse.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@ComponentScan(basePackages = "lk.ijse.spring")
+public class AppConfig {
+}
---
+++ SpringFramework/03-Bean_Scopes/src/lk/ijse/spring/pojo/SpringBeanOne.java
@@ -0,0 +1,45 @@
+package lk.ijse.spring.pojo;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("singleton")
+public class SpringBeanOne implements BeanNameAware,BeanFactoryAware, ApplicationContextAware, InitializingBean,DisposableBean {
+
+    public SpringBeanOne(){
+        System.out.println("SpringBeanOne : Instantiated");
+    }
+
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("Bean Name Aware");
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("Bean Factory Aware");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("Application Context Aware");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Initializing Bean : Bean is ready to use");
+    }
+
+
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("SpringBeanOne : Destroyed");
+    }
+}
