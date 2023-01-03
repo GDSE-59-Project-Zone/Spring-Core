3cece67aad0cdd43a01bde1d34036c04f4084032
sanuv9683 <sanuv9683@gmail.com>
2023-01-03 10:23:25 +0530

    Added lifecycle methods for a and b

--- SpringFramework/04_DI_Spring/src/lk/ijse/spring/pojo/A.java
+++ SpringFramework/04_DI_Spring/src/lk/ijse/spring/pojo/A.java
@@ -1,10 +1,16 @@
 package lk.ijse.spring.pojo;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class A {
+public class A implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
 
+    @Autowired
     private B b;
 
     public A(){
@@ -13,5 +19,30 @@ public class A {
 
     public void sendRequestForB(){
     b.callMe();
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("Bean Factory Aware: A");
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("Bean Name Aware: A");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("Disposable Bean: A");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Initializing Bean: A");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("Application Context Aware: A");
     }
 }
--- SpringFramework/04_DI_Spring/src/lk/ijse/spring/pojo/B.java
+++ SpringFramework/04_DI_Spring/src/lk/ijse/spring/pojo/B.java
@@ -1,14 +1,43 @@
 package lk.ijse.spring.pojo;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class B {
+public class B implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
     public B(){
         System.out.println("Instantiated: B");
     }
 
     public void callMe(){
         System.out.println("Received the call");
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("Bean Factory Aware: A");
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("Bean Name Aware: A");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("Disposable Bean: A");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Initializing Bean: A");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("Application Context Aware: A");
     }
 }
