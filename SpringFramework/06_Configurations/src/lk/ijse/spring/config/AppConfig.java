02247931c2f67ab73434b0083fa24db49bbfa586
sanuv9683 <sanuv9683@gmail.com>
2023-01-10 09:00:14 +0530

    Introducing more than one configuration class to the container

--- SpringFramework/06_Configurations/src/lk/ijse/spring/config/AppConfig.java
+++ SpringFramework/06_Configurations/src/lk/ijse/spring/config/AppConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "lk.ijse.spring")
+@ComponentScan(basePackages = "lk.ijse.spring.pojo")
 public class AppConfig {
+    public AppConfig(){
+        System.out.println("AppConfig:Instantiated");
+    }
 }
---
+++ SpringFramework/06_Configurations/src/lk/ijse/spring/config/ConfigOne.java
@@ -0,0 +1,11 @@
+package lk.ijse.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigOne {
+
+    public ConfigOne(){
+        System.out.println("ConfigOne:Instantiated");
+    }
+}
