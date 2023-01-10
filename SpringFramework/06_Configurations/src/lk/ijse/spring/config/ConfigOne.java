42bb2d4e856ce8f128773b3767dde461b9261d91
sanuv9683 <sanuv9683@gmail.com>
2023-01-10 09:05:00 +0530

    Content Updated with Bean Introducing

--- SpringFramework/06_Configurations/src/lk/ijse/spring/config/ConfigOne.java
+++ SpringFramework/06_Configurations/src/lk/ijse/spring/config/ConfigOne.java
@@ -1,5 +1,7 @@
 package lk.ijse.spring.config;
 
+import lk.ijse.spring.pojo.SBeanTwo;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -7,5 +9,10 @@ public class ConfigOne {
 
     public ConfigOne(){
         System.out.println("ConfigOne:Instantiated");
+    }
+
+    @Bean
+    public SBeanTwo beanTwo(){
+        return new SBeanTwo();
     }
 }
