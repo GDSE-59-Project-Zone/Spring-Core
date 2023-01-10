4730eeb8ac42b001bfdc043d25f5fffa736f14c2
sanuv9683 <sanuv9683@gmail.com>
2023-01-10 11:19:46 +0530

    Usage of Spring Environment

--- SpringFramework/08_SpringEnvironment/src/lk/ijse/spring/pojo/PojoOne.java
+++ SpringFramework/08_SpringEnvironment/src/lk/ijse/spring/pojo/PojoOne.java
@@ -1,10 +1,24 @@
 package lk.ijse.spring.pojo;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class PojoOne {
+public class PojoOne  implements InitializingBean {
+
+    @Value("${os.name}")
+    private String myOSName;
+    @Value("${LOGNAME}")//${} property placeholder
+    private String myLogName;
+
     public PojoOne(){
         System.out.println("PojoOne:Instantiated");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println(myOSName);
+        System.out.println(myLogName);
     }
 }
