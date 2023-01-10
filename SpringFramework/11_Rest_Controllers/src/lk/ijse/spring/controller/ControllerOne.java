e28d02a5babf048fdd625ef95a3bae28a658c634
sanuv9683 <sanuv9683@gmail.com>
2023-01-10 16:57:11 +0530

    Content Updated

--- SpringFramework/11_Rest_Controllers/src/lk/ijse/spring/controller/ControllerOne.java
+++ SpringFramework/11_Rest_Controllers/src/lk/ijse/spring/controller/ControllerOne.java
@@ -1,6 +1,7 @@
 package lk.ijse.spring.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +14,12 @@ public class ControllerOne {
     public String getMethod(){
         return "Get Method";
     }
+
+
+    @PostMapping
+    public String postMethod(){
+        return "Post Method";
+    }
+
+
 }
