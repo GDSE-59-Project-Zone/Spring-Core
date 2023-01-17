2ed11d0ffc0da7177097eea363e09bc43e5b5e04
sanuv9683 <sanuv9683@gmail.com>
2023-01-17 09:32:56 +0530

    Basic Mapping methods in Spring

--- SpringFramework/12_Mapping_Spec/src/lk/ijse/spring/controller/ControllerOne.java
+++ SpringFramework/12_Mapping_Spec/src/lk/ijse/spring/controller/ControllerOne.java
@@ -1,15 +1,32 @@
 package lk.ijse.spring.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/one")
 public class ControllerOne {
 
     @GetMapping
-    public void testOne(){
+    public String testOne(){
         System.out.println("Get Mapping Invoked");
+        return "Get Mapping Was Invoked";
+    }
+
+    @PostMapping
+    public String testTwo(){
+        System.out.println("Post Mapping Invoked");
+        return "Post Mapping Was Invoked";
+    }
+
+    @DeleteMapping
+    public String testThree(){
+        System.out.println("Delete Mapping Invoked");
+        return "Delete Mapping Was Invoked";
+    }
+
+    @PutMapping
+    public boolean testFour(){
+        System.out.println("Put Mapping Invoked");
+        return false; // need converters
     }
 }
