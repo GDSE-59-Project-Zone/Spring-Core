00f573339ca6b3fecc0fa04507cb1603d5a8ab96
sanuv9683 <sanuv9683@gmail.com>
2023-01-17 11:10:43 +0530

    Cosume type header for request narrow down

--- SpringFramework/12_Mapping_Spec/src/lk/ijse/spring/controller/ControllerSix.java
+++ SpringFramework/12_Mapping_Spec/src/lk/ijse/spring/controller/ControllerSix.java
@@ -2,6 +2,7 @@ package lk.ijse.spring.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,13 +11,13 @@ public class ControllerSix {
 
     //mapping with query parameters
     //we can narrow down spring request using Query params
-
+    //We can use @RequestParam annotation to catch query parameters values
 
     //http://localhost:8080/mapping/six?id=C001
     //id, name
     @GetMapping(params = {"id","name"})
-    public String testOne(String id,String name){
-        return "Test One Invoked" +id+" : "+name;
+    public String testOne(@RequestParam("id") String myID,@RequestParam String name){
+        return "Test One Invoked" +myID+" : "+name;
     }
 
     //address,salary
