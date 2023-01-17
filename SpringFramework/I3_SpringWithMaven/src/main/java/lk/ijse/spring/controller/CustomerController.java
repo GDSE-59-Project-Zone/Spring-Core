84b8a87f1b3b064607885c6b8d977fdef17cd216
sanuv9683 <sanuv9683@gmail.com>
2023-01-17 16:06:06 +0530

    Content Updated with @CrossOrigin annotation

--- SpringFramework/I3_SpringWithMaven/src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ SpringFramework/I3_SpringWithMaven/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -1,12 +1,21 @@
 package lk.ijse.spring.controller;
 
 
+import lk.ijse.spring.dto.CustomerDTO;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/customer")
+@CrossOrigin//this will handle all the cross policy errors
 public class CustomerController {
 
 
+    @PostMapping
+    public String saveCustomer(CustomerDTO dto){
+        System.out.println(dto.toString());
+        return "Customer Added";
+    }
 }
