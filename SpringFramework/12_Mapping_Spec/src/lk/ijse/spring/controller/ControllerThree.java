50cdc77128a500cb6e9e5dbfb22f4559a5180e6f
sanuv9683 <sanuv9683@gmail.com>
2023-01-17 10:35:32 +0530

    Wild Card Mapping

--- SpringFramework/12_Mapping_Spec/src/lk/ijse/spring/controller/ControllerThree.java
+++ SpringFramework/12_Mapping_Spec/src/lk/ijse/spring/controller/ControllerThree.java
@@ -1,5 +1,6 @@
 package lk.ijse.spring.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/three")
 public class ControllerThree {
 
-    //Mapping Spec FutherMore
+    //Mapping Spec FurtherMore
+    //Character matcher in path segments
+    @GetMapping(path = "C???1")
+    public String  testOne(){
+        return "Test One Invoked";
+    }
+
+    @GetMapping(path = "C??1")
+    public String  testTwo(){
+        return "Test Two Invoked";
+    }
+
 }
