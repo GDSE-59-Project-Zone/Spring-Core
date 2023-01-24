6bba0be4d3e1ddbd6c23138ccafcdc769a13ed7e
sanuv9683 <sanuv9683@gmail.com>
2023-01-24 14:19:25 +0530

    Introduced lombok to the project

--- SpringFramework/I5_Spring_Data_JPA/src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ SpringFramework/I5_Spring_Data_JPA/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -2,42 +2,50 @@ package lk.ijse.spring.controller;
 
 
 import lk.ijse.spring.dto.CustomerDTO;
+import lk.ijse.spring.entity.Customer;
+import lk.ijse.spring.repo.CustomerRepo;
 import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/customer")
 @CrossOrigin//this will handle all the cross policy errors
 public class CustomerController {
 
+    @Autowired
+    CustomerRepo repo;
 
     @PostMapping
     public ResponseUtil saveCustomer(@ModelAttribute CustomerDTO dto){
         System.out.println(dto.toString());
+        Customer customer = new Customer(dto.getId(), dto.getName(),dto.getAddress(), dto.getSalary());
+        repo.save(customer);
+
         return new ResponseUtil("200",dto.toString()+ " Added",null);
     }
 
     @PutMapping
     public ResponseUtil updateCustomer(@RequestBody CustomerDTO dto){
         System.out.println(dto.toString());
+        Customer customer = new Customer(dto.getId(), dto.getName(),dto.getAddress(), dto.getSalary());
+        repo.save(customer);
         return new ResponseUtil("200",dto.toString()+" Updated",null);
     }
 
     @DeleteMapping(params = "id")
     public ResponseUtil deleteCustomer(String id){
         System.out.println(id);
+        repo.deleteById(id);
         return new ResponseUtil("200",id+" Deleted",null);
     }
 
     @GetMapping
     public ResponseUtil getAllCustomer(){
-        ArrayList<CustomerDTO> allCustomers= new ArrayList<>();
-        allCustomers.add(new CustomerDTO("C001","Kasun","Galle",1000));
-        allCustomers.add(new CustomerDTO("C002","Ramal","Panadura",2000));
-        allCustomers.add(new CustomerDTO("C003","Yasith","Kaluthara",3000));
-        allCustomers.add(new CustomerDTO("C004","Ranuka","Bandarawela",4000));
-        return new ResponseUtil("200"," Success",allCustomers);
+        List<Customer> all = repo.findAll();
+        return new ResponseUtil("200"," Success",all);
     }
 }
--- SpringFramework/I5_Spring_Data_JPA/src/main/java/lk/ijse/spring/entity/Customer.java
+++ SpringFramework/I5_Spring_Data_JPA/src/main/java/lk/ijse/spring/entity/Customer.java
@@ -1,8 +1,17 @@
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 @Entity
 public class Customer {
     @Id
@@ -11,55 +20,5 @@ public class Customer {
     private String address;
     private double salary;
 
-    public Customer() {
-    }
 
-    public Customer(String id, String name, String address, double salary) {
-        this.id = id;
-        this.name = name;
-        this.address = address;
-        this.salary = salary;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
-
-    @Override
-    public String toString() {
-        return "Customer{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                ", address='" + address + '\'' +
-                ", salary=" + salary +
-                '}';
-    }
 }
