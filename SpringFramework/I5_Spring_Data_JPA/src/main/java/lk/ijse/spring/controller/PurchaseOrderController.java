d646f5da9f0bdd151557e6d4c5e096a0d3391748
sanuv9683 <sanuv9683@gmail.com>
2023-01-30 20:18:36 +0530

    Added purcahse controller

---
+++ SpringFramework/I5_Spring_Data_JPA/src/main/java/lk/ijse/spring/controller/PurchaseOrderController.java
@@ -0,0 +1,17 @@
+package lk.ijse.spring.controller;
+
+import lk.ijse.spring.dto.OrdersDTO;
+import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/purchase")
+@CrossOrigin
+public class PurchaseOrderController {
+
+    @PostMapping
+    public ResponseUtil purchaseOrder(@RequestBody OrdersDTO dto){
+        System.out.println(dto.toString());
+        return new ResponseUtil("200","Successfully Purchased.!",null);
+    }
+}
