94a8a660d352e3aaacfdd6d60dce1a193b0211a9
sanuv9683 <sanuv9683@gmail.com>
2023-01-31 11:56:24 +0530

    Write down a test method for Customer Repo

--- SpringFramework/I5_Spring_Data_JPA/src/test/java/lk/ijse/spring/repo/CustomerRepoTest.java
+++ SpringFramework/I5_Spring_Data_JPA/src/test/java/lk/ijse/spring/repo/CustomerRepoTest.java
@@ -1,18 +1,33 @@
 package lk.ijse.spring.repo;
 
 import lk.ijse.spring.config.WebRootConfig;
+import lk.ijse.spring.entity.Customer;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @WebAppConfiguration
 @ContextConfiguration(classes = {WebRootConfig.class})
 @ExtendWith(SpringExtension.class)
 class CustomerRepoTest {
 
+    @Autowired
+    CustomerRepo customerRepo;
+
+    @Test
+    public void testOne() {
+        Customer data = customerRepo.findCustomerByName("Ra");
+        System.out.println(data.toString());
+    }
+
+    @Test
+    public void testTwo() {
+        Customer data = customerRepo.findCustomerByNameAndAddress("Thamalsha","Galle");
+        System.out.println(data.toString());
+    }
 
 
 }
